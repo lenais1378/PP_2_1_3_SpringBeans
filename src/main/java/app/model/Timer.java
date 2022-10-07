402ad44fe90ab190fa4b9bfdb67fdf7a269843c8
lenais1378 <lenais1378@gmail.com>
2022-10-07 16:17:27 +0300

    little changes timer

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("singleton")
+
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
